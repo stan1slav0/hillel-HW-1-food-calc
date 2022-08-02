515a3840c19edcbf61445434915cecf5ad2adb62
Stanislav <pingo.mw2@gmail.com>
2022-08-02 22:43:16 +0300

    food calc first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+apple = `🍎`;
+applePrice = 10;
+appleCount = 3;
+appleSalePercent = 7;
+
+const appleResultPercent = applePrice / 100 * appleSalePercent;
+const appleFinalPrice = applePrice * appleCount - appleResultPercent * appleCount;
+
+orange = `🍊`;
+orangePrice = 12;
+orangeCount = 2;
+orangeSalePercent = 3;
+
+const orangeResultPercent = orangePrice / 100 * orangeSalePercent;
+const orangeFinalPrice = orangePrice * orangeCount - orangeResultPercent * orangeCount;
+
+kiwi = `🥝`;
+kiwiPrice = 15;
+kiwiCount = 10;
+kiwiCountryPercent = 10;
+
+const kiwiResultPercent = kiwiPrice / 100 * kiwiCountryPercent;
+const kiwiFinalPrice = kiwiPrice * kiwiCount + kiwiResultPercent * kiwiCount;
+
+
+const day = new Date().getDate();
+const month = new Date().getMonth();
+const year = new Date().getFullYear();
+
+
+let captionString = `Food prices ≠ ${day}.${month + 1}.${year}`;
+
+
+console.log(captionString.replace(`≠`,`-`));
+
+console.log(`Final price for ${appleCount} ${apple} = ${appleFinalPrice} UAH`);
+console.log(`Final price for ${orangeCount} ${orange} = ${orangeFinalPrice} UAH`);
+console.log(`Final price for ${kiwiCount} ${kiwi} = ${kiwiFinalPrice} UAH`);
+console.log(`Final price for all products = ${appleFinalPrice + orangeFinalPrice + kiwiFinalPrice} UAH`)
+
+
